1e1e7349846ecb2374f57ed730fe188387ee02c9
helton-evambi <heltonevambi@gmail.com>
2026-02-05 21:53:45 +0100

    feat: create data context

---
+++ src/Clientes.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Clientes.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clientes.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Cliente> Clientes { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options)
+    {
+    }
+}
