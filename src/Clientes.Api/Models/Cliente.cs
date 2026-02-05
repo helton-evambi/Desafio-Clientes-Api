f1c1613f62cb9ddcf0d2bd32df59b75e26c139d4
helton-evambi <heltonevambi@gmail.com>
2026-02-05 21:54:20 +0100

    feat: create client model

---
+++ src/Clientes.Api/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clientes.Api.Models;
+
+public class Cliente
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+    public string Nome { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo Email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+    public string Email { get; set; } = string.Empty;
+}
